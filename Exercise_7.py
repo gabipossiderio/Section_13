a79f7ee7a6d4632efd378c967af1c1dcce40a2d1
Gabi Possiderio <gabipossiderio@gmail.com>
2022-04-11 00:19:22 -0300

    Changed Exercise 7

--- Exercise_7.py
+++ Exercise_7.py
@@ -1,14 +1,14 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 
-with open('arq.txt', 'r+') as arquivo:
-    arquivo_old = arquivo.read()
-    arquivo_new = ''
-    print(f'File before changes: \n{arquivo_old}')
-    for character in arquivo_old:
+with open('file1.txt', 'r+') as old_file:
+    file_content = old_file.read()
+    modified_content = ''
+    print(f'File before changes: \n{file_content}')
+    for character in file_content:
         if character.lower() in vowels:
             character = "*"
-        arquivo_new += character
-    arquivo.seek(0)
-    arquivo.write(arquivo_new)
-    arquivo.seek(0)
-    print(f'\nFile after vowels are replaced by (*): \n{arquivo.read()}')
+        modified_content += character
+with open('file2.txt', 'r+') as new_file:
+    new_file.write(modified_content)
+    new_file.seek(0)
+    print(f'\nFile after vowels are replaced by (*): \n{new_file.read()}')
